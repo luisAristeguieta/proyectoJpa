d68ceda57863e83fa8888c1cdcdf830ba561c7bc
luisAristeguieta <luisjimenezgx@gmail.com>
2026-06-06 17:24:30 -0400

    first commit

---
+++ src/main/java/com/krakedev/proyectos/ProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectosApplication.class, args);
+	}
+
+}
